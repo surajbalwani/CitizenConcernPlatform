2ba35ef8d11eb43a1fe50776d84348151e976889
CORESVCS\Suraj.Balwani <suraj.balwani@civica.com>
2025-09-09 10:29:10 -1200

    use swagger regardless of environment

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -102,11 +102,11 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 app.UseCors("AllowAll");
