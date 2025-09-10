62d654cef1a934bfa6c765bda34ef431c581c27d
CORESVCS\Suraj.Balwani <suraj.balwani@civica.com>
2025-09-10 02:27:51 -1200

    .

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<app-name>'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/<app-name>/index.html'));});
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 app.listen(process.env.PORT || 8080);
